d513ecab44078bfa83e5318051c117e16e6d854d
crac <40260333+cracit@users.noreply.github.com>
2020-05-19 17:17:38 +0300

    3

--- cmd/terraform-provider-proxmox/main.go
+++ cmd/terraform-provider-proxmox/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/cracit/terraform-provider-proxmox/tree/master/proxmox"
+	"github.com/cracit/terraform-provider-proxmox/proxmox"
 	"github.com/hashicorp/terraform/tree/master/plugin"
 	"github.com/hashicorp/terraform/terraform"
 )
